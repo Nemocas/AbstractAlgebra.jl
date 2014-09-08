44da191520f105e3da7018ce23e75dec0d7d45e5
William Hart <goodwillhart@googlemail.com>
2014-09-08 11:30:37 +0100

    Added tests from manual for residue rings.

--- Rings-test.jl
+++ Rings-test.jl
@@ -6,10 +6,12 @@ export test_all
 
 include("ZZ-test.jl")
 include("Poly-test.jl")
+include("Residue-test.jl")
 
 function test_all()
    test_zz()
    test_poly()
+   test_residue()
 end
 
 end # module
