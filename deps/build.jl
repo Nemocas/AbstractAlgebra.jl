e14e06c39e536aa6b8a1efb1ae8e865be6fce37a
William Hart <goodwillhart@googlemail.com>
2014-09-13 00:22:42 +0100

    Yet another attempt at fixing the Windows woes.

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,8 @@ cd(pwd)
 on_windows = @windows ? true : false
 
 if on_windows
+   start = ENV["COMSPEC"]
+   
    if Int == Int32
       ENV["MSYSTEM"]="MINGW32"
    else
@@ -25,16 +27,18 @@ cd("$pwd/mpir-2.7.0")
 if on_windows
    run(`sed -i 's/data.rel.ro.local,"aw",@progbits/data.rel.ro.local,"aw"/g' $pwd/mpir-2.7.0/mpn/x86_64/x86_64-defs.m4`)
    if Int == Int32
-      run(`bash configure --prefix=$pwd --enable-gmpcompat --disable-static --enable-shared ABI=32`)
+      run(`$start /e:4096 /c sh configure --prefix=$pwd --enable-gmpcompat --disable-static --enable-shared ABI=32`)
    else
-      run(`bash configure --prefix=$pwd --enable-gmpcompat --disable-static --enable-shared ABI=64`)
+      run(`$start /e:4096 /c sh configure --prefix=$pwd --enable-gmpcompat --disable-static --enable-shared ABI=64`)
    end
+   run(`$start /e:4096 /c sh make -j4`)
+   run(`$start /e:4096 /c sh make install`)
 else
    run(`./configure --prefix=$pwd --enable-gmpcompat --disable-static --enable-shared`)
+   run(`make -j4`)
+   run(`make install`)
 end
 
-run(`make -j4`)
-run(`make install`)
 cd(pwd)
 
 # install MPFR
@@ -45,13 +49,15 @@ run(`rm mpfr-3.1.2.tar.bz2`)
 cd("$pwd/mpfr-3.1.2")
 
 if on_windows
-   run(`bash configure --prefix=$pwd --with-gmp=$pwd --disable-static --enable-shared`)
+   run(`$start /e:4096 /c sh configure --prefix=$pwd --with-gmp=$pwd --disable-static --enable-shared`)
+   run(`$start /e:4096 /c sh make -j4`)
+   run(`$start /e:4096 /c sh make install`)
 else
    run(`./configure --prefix=$pwd --with-gmp=$pwd --disable-static --enable-shared`)
+   run(`make -j4`)
+   run(`make install`)
 end
 
-run(`make -j4`)
-run(`make install`)
 cd(pwd)
 
 # install FLINT
@@ -61,16 +67,18 @@ cd("$pwd/flint2")
 
 if on_windows
    if Int == Int32
-      run(`bash configure --prefix=$pwd --disable-static --enable-shared --with-mpir=$pwd --with-mpfr=$pwd ABI=32`)
+      run(`$start /e:4096 /c sh sh configure --prefix=$pwd --disable-static --enable-shared --with-mpir=$pwd --with-mpfr=$pwd ABI=32`)
    else
-      run(`bash configure --prefix=$pwd --disable-static --enable-shared --with-mpir=$pwd --with-mpfr=$pwd ABI=64`)
+      run(`$start /e:4096 /c sh sh configure --prefix=$pwd --disable-static --enable-shared --with-mpir=$pwd --with-mpfr=$pwd ABI=64`)
    end
+   run(`$start /e:4096 /c sh make -j4`)
+   run(`$start /e:4096 /c sh make install`)
 else
    run(`./configure --prefix=$pwd --disable-static --enable-shared --with-mpir=$pwd --with-mpfr=$pwd`)
+   run(`make -j4`)
+   run(`make install`)
 end
 
-run(`make -j4`)
-run(`make install`)
 cd(pwd)
 
 if on_windows
