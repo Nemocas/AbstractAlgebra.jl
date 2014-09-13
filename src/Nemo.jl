48548be7fd8249276f05d13875e4f367dbc2088e
William Hart <goodwillhart@googlemail.com>
2014-09-13 09:39:16 +0100

    Pkgdir.

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -5,9 +5,9 @@ pkgdir = Pkg.dir("Nemo")
 on_windows = @windows ? true : false
 
 if on_windows
-   push!(DL_LOAD_PATH, "$pkdir\\src\\lib")
+   push!(DL_LOAD_PATH, "$pkgdir\\src\\lib")
 else
-   push!(DL_LOAD_PATH, "$pkdir/src/lib")
+   push!(DL_LOAD_PATH, "$pkgdir/src/lib")
 end
 
 include("Rings.jl")
