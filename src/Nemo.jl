c2c53f2715657be1adb9bfd55ad047b8176f1794
William Hart <goodwillhart@googlemail.com>
2015-01-13 21:42:17 +0000

    Corrent syntax.

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -6,16 +6,20 @@ on_windows = @windows ? true : false
 
 if on_windows
    push!(DL_LOAD_PATH, "$pkgdir\\src\\lib")
-else                                                                               try                                                                                if ENV["HOSTNAME"] == "juliabox"
+else
+   try
+      if ENV["HOSTNAME"] == "juliabox"
          push!(DL_LOAD_PATH, "/usr/local/lib")
          dlopen("/usr/local/lib/libgmp.so")
          dlopen("/usr/local/lib/libmpfr.so")
          dlopen("/usr/local/lib/libflint.so")
       else
          push!(DL_LOAD_PATH, "$pkgdir/src/lib")
+      end
    catch
       push!(DL_LOAD_PATH, "$pkgdir/src/lib")
-   end                                                                          end     
+   end
+end
 
 include("Rings.jl")
 include("Fields.jl")
