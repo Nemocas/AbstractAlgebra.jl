cedec5c58e632b7c3e7b75ed28f6d0de77d088c9
William Hart <goodwillhart@googlemail.com>
2015-04-10 12:31:17 +0100

    Move factor functionality out into separate file, put back old ZZ module.

--- src/Rings.jl
+++ src/Rings.jl
@@ -1,14 +1,21 @@
-###########################################################################################
+###############################################################################
 #
 #   Rings.jl : Generic rings
 #
-###########################################################################################
+###############################################################################
 
-import Base: length, call, exp, promote_rule, zero, one, show, divrem, mod, hash, factor
+import Base: length, call, exp, promote_rule, zero, one, show, divrem, mod, 
+             hash, factor
 
 export Ring, Field, RingElem
 
 export PolyElem
+
+###############################################################################
+#
+#   Abstract types
+#
+###############################################################################
 
 abstract Ring
 
@@ -22,6 +29,12 @@ abstract PolyElem <: RingElem
 
 # not always mathematical ring elements
 abstract MatElem <: RingElem
+
+###############################################################################
+#
+#   Generic catchall functions
+#
+###############################################################################
 
 function +{S <: RingElem, T <: RingElem}(x::S, y::T) 
    T1 = promote_type(S, T)
@@ -139,83 +152,45 @@ function =={S <: Integer, T <: RingElem}(x::S, y::T)
    end
 end
 
+###############################################################################
+#
+#   Generic and specific rings and fields
+#
+###############################################################################
+
 include("ZZ.jl")
 
-include("Residue.jl")
+# include("Residue.jl")
 
-include("Poly.jl")
+# include("Poly.jl")
 
-include("fmpz_poly.jl")
+# include("fmpz_poly.jl")
 
-include("fmpz_mat.jl")
+# include("fmpz_mat.jl")
 
-include("PariRings.jl")
+# include("PariRings.jl")
 
-include("Fields.jl")
+# include("Fields.jl")
 
-include("PariFields.jl")
+# include("PariFields.jl")
 
-include("Poly2.jl")
+# include("Poly2.jl")
 
-include("pari_poly2.jl")
+# include("pari_poly2.jl")
 
-include("NumberFields.jl")
+# include("NumberFields.jl")
 
-include("MaximalOrders.jl")
+# include("MaximalOrders.jl")
 
-include("PariIdeal.jl")
+# include("PariIdeal.jl")
 
-###########################################################################################
+# include("Factor.jl")
+
+###############################################################################
 #
-#   Factorisation
+#   Test code
 #
-###########################################################################################
-
-type Factor{T <: Ring}
-   d::PariFactor
-   len::Int
-   parent::T
-end
-
-function getindex(a::Factor{IntegerRing}, i::Int)
-   p, n = a.d[i]
-   return ZZ(p), n
-end
-
-function getindex{S}(a::Factor{FmpzPolyRing{S}}, i::Int)
-   p, n = a.d[i]
-   return a.parent(p), n
-end
-
-function getindex{S}(a::Factor{FmpqPolyRing{S}}, i::Int)
-   p, n = a.d[i]
-   return a.parent(p), n
-end
-
-function show(io::IO, a::Factor)
-   print(io, "[")
-   for i = 1:a.len
-      print(io, a[i])
-      if i != a.len
-         print(io, ", ")
-      end
-   end
-   print(io, "]")
-end
-
-function factor(n::BigInt)
-   f = factor(pari(n))
-   return Factor{IntegerRing}(f, f.len, ZZ)
-end
-
-function factor{S}(g::fmpz_poly{S})
-   f = factor(pari(g))
-   return Factor{FmpzPolyRing{S}}(f, f.len, g.parent)
-end
-function factor{S}(g::fmpq_poly{S})
-   f = factor(pari(g))
-   return Factor{FmpqPolyRing{S}}(f, f.len, g.parent)
-end
+###############################################################################
 
 include("../test/Rings-test.jl")
 
