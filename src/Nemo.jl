968471bded4610921af66d2f947bce59c1738f2e
William Hart <goodwillhart@googlemail.com>
2015-12-04 14:31:34 +0000

    Update version number to Nemo 0.4.0.

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -97,7 +97,7 @@ function __init__()
    unsafe_store!(pari_sigint, cfunction(pari_sigint_handler, Void, ()), 1)
 
    println("")
-   println("Welcome to Nemo version 0.3.1")
+   println("Welcome to Nemo version 0.4.0")
    println("")
    println("Nemo comes with absolutely no warranty whatsoever")
    println("")
