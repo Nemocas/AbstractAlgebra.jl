28e17e1e88d9644f3606c466f0c6e154869af5e3
Tommy Hofmann <thofma@gmail.com>
2016-03-16 20:49:02 +0100

    Ad hoc comparison for fmpz and UInt

--- src/flint/fmpz.jl
+++ src/flint/fmpz.jl
@@ -600,6 +600,30 @@ end
 
 >(x::Int, y::fmpz) = cmp(y,x) < 0
 
+function cmp(x::fmpz, y::UInt)
+    Int(ccall((:fmpz_cmp_ui, :libflint), Cint, (Ptr{fmpz}, UInt), &x, y))
+end
+
+==(x::fmpz, y::UInt) = cmp(x,y) == 0
+
+<=(x::fmpz, y::UInt) = cmp(x,y) <= 0
+
+>=(x::fmpz, y::UInt) = cmp(x,y) >= 0
+
+<(x::fmpz, y::UInt) = cmp(x,y) < 0
+
+>(x::fmpz, y::UInt) = cmp(x,y) > 0
+
+==(x::UInt, y::fmpz) = cmp(y,x) == 0
+
+<=(x::UInt, y::fmpz) = cmp(y,x) >= 0
+
+>=(x::UInt, y::fmpz) = cmp(y,x) <= 0
+
+<(x::UInt, y::fmpz) = cmp(y,x) > 0
+
+>(x::UInt, y::fmpz) = cmp(y,x) < 0
+
 ###############################################################################
 #
 #   Bit fiddling
