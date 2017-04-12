636b0906dd25d2955ce800a25a3d1608f8ea65f4
Johannes Schmitt <jschmitt@posteo.eu>
2017-04-12 15:15:45 +0200

    Restructured weak_popov! and added rank_profile_popov.

--- src/generic/Matrix.jl
+++ src/generic/Matrix.jl
@@ -3293,7 +3293,40 @@ function weak_popov_with_pivots!{T <: PolyElem}(P::GenMat{T}, W::GenMat{T}, U::G
          end
       end
    end
-   return nothing
+   return return_pivots == Val{true} ? pivots : nothing
+end
+
+function rank_profile_popov{T <: PolyElem}(A::GenMat{T})
+   B = deepcopy(A)
+   m = rows(A)
+   n = cols(A)
+   U = zero(MatrixSpace(base_ring(A), 0, 0))
+   r = 0
+   rank_profile = Array{Int,1}(0)
+   pivots = Array{Array{Int,1}}(n)
+   for i = 1:n
+      pivots[i] = Array{Int}(0)
+   end
+   p = find_pivot_popov(B, 1)
+   if B[1,p] != 0
+      push!(pivots[p], 1)
+      r = 1
+      push!(rank_profile, 1)
+   end
+   for i = 2:m
+      p = find_pivot_popov(B, i)
+      B[i,p] != 0 ? push!(pivots[p], i) : nothing
+      pivots = weak_popov_with_pivots!(B, U, pivots, false, i, Val{true})
+      s = 0
+      for j = 1:n
+         length(pivots[j]) != 0 ? s += 1 : nothing
+      end
+      if s != r
+         push!(rank_profile, i)
+         r = s
+      end
+   end
+   return rank_profile
 end
 
 function rank_profile_popov{T <: PolyElem}(A::GenMat{T})
