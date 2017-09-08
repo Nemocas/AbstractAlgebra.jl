b77002c011688d26d7bfd74afa9c876f32e204ad
William Hart <goodwillhart@googlemail.com>
2017-09-08 09:19:43 +0100

    Export NumberField and remove unneeded assignments at beginning of tests.

--- test/Nemo-test.jl
+++ test/Nemo-test.jl
@@ -3,15 +3,6 @@ include("Rings-test.jl")
 include("Fields-test.jl")
 include("Benchmark-test.jl")
 
-ZZ = Nemo.ZZ
-QQ = Nemo.QQ
-FiniteField = Nemo.FiniteField
-PadicField = Nemo.PadicField
-NumberField = Nemo.NumberField
-CyclotomicField = Nemo.CyclotomicField
-MaximalRealSubfield = Nemo.MaximalRealSubfield
-PermutationGroup = Nemo.PermutationGroup
-
 function test_all()
    test_groups()
    test_rings()
