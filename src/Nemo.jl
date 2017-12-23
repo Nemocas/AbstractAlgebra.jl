de569957c6c779c199d2a3f48c710fd6f88b7ed0
thofma <thofma@gmail.com>
2017-12-23 10:03:09 +0100

    Bump version number of Nemo

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -117,7 +117,7 @@ function __init__()
       (Ptr{Void},), cfunction(flint_abort, Void, ()))
 
    println("")
-   println("Welcome to Nemo version 0.7.0")
+   println("Welcome to Nemo version 0.7.1")
    println("")
    println("Nemo comes with absolutely no warranty whatsoever")
    println("")
@@ -138,7 +138,7 @@ end
 ################################################################################
 
 function versioninfo()
-  print("Nemo version 0.7.0 \n")
+  print("Nemo version 0.7.1 \n")
   nemorepo = dirname(dirname(@__FILE__))
 
   print("Nemo: ")
