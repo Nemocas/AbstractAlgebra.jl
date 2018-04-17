cdc2447640f24f76ea6b4fafa77cec1e33cf9a98
William Hart <goodwillhart@googlemail.com>
2018-04-17 13:56:29 +0200

    Allow enable_cache! and disable_cache!

--- src/generic/MapCache.jl
+++ src/generic/MapCache.jl
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-export cached, set_limit!
+export cached, disable_cache!, enable_cache!, set_limit!
 
 get_field(M::MapCache, f) = getfield(M.map, f) # pass accessors through
 set_field!(M::MapCache, f) = setfield(M.map, f) # pass accessors through
 
-function cached(M::AbstractAlgebra.Map; limit::Int = 100, enable::Bool = true)
-   return MapCache(M, limit, enable)
+function cached(M::AbstractAlgebra.Map; limit::Int = 100, enabled::Bool = true)
+   return MapCache(M, limit, enabled)
 end
 
 domain(M::MapCache) = domain(M.map)
@@ -19,6 +19,19 @@ codomain(M::MapCache) = codomain(M.map)
 function set_limit!(M::MapCache, limit::Int)
    limit < 0 && error("Limit must be nonnegative")
    M.limit = limit
+end
+
+function enable_cache!(M::MapCache{D, C, S, T, De, Ce}) where {D, C, S, T, De, Ce}
+   if !isdefined(M, :image_cache)
+      M.image_cache = Dict{De, Ce}()
+   end
+   M.enabled = true
+   nothing
+end
+
+function disable_cache!(M::MapCache)
+   M.enabled = false
+   nothing
 end
 
 function (M::MapCache{D, C, S, T, De, Ce})(a::De) where {D, C, S, T, De, Ce}
