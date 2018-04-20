3257cef958d014ed62e2f628e31ce2a065c5f8fa
kalmarek <kalmar@amu.edu.pl>
2018-04-20 14:41:04 +0200

    run each test for all reasonable Integer types

--- test/generic/Perm-test.jl
+++ test/generic/Perm-test.jl
@@ -8,253 +8,269 @@ function test_perm_abstract_types()
    println("PASS")
 end
 
+types = [Int8, Int16, Int32, Int, UInt8, UInt16, UInt32, UInt]
+
 function test_perm_constructors()
-   print("perm.constructors...")
+   print(rpad("perm.constructors...", 30))
 
-   @test elem_type(Generic.PermGroup{Int}) == Generic.perm{Int}
-   @test parent_type(Generic.perm{Int}) == Generic.PermGroup{Int}
+   for T in types
+      print("$T ")
+      @test elem_type(Generic.PermGroup{T}) == Generic.perm{T}
+      @test parent_type(Generic.perm{T}) == Generic.PermGroup{T}
 
-   R = PermutationGroup(10)
-   @test typeof(R) == Generic.PermGroup{Int}
-   @test elem_type(R) == Generic.perm{Int}
+      @test PermutationGroup(T(10)) isa Generic.PermGroup{T}
+      R = PermutationGroup(T(10))
+      @test elem_type(R) == Generic.perm{T}
 
-   a = R()
-   @test typeof(a) == Generic.perm{Int}
-   @test parent_type(typeof(a)) == Generic.PermGroup{Int}
+      @test R() isa GroupElem
+      @test R() isa Generic.perm{T}
+      a = R()
+      @test parent_type(typeof(a)) == Generic.PermGroup{T}
+      @test parent(a) == R
 
-   b = R([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
-   @test typeof(b) == Generic.perm{Int}
-   @test parent_type(typeof(b)) == Generic.PermGroup{Int}
+      @test R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8]) isa GroupElem
+      @test R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8]) isa Generic.perm{T}
+      b = R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
+      @test typeof(b) == Generic.perm{T}
+      @test parent_type(typeof(b)) == Generic.PermGroup{T}
+      @test parent(b) == R
 
-
-   R = PermutationGroup(Int16(10))
-   @test typeof(R) == Generic.PermGroup{Int16}
-   @test elem_type(R) == Generic.perm{Int16}
-
-   a = R()
-   @test typeof(a) == Generic.perm{Int16}
-   @test parent_type(typeof(a)) == Generic.PermGroup{Int16}
-
-   b = R(Int16[2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
-   @test typeof(b) == Generic.perm{Int16}
-   @test parent_type(typeof(b)) == Generic.PermGroup{Int16}
-
-   c = R(a)
-
-   @test isa(a, GroupElem)
-   @test isa(b, GroupElem)
-   @test isa(c, GroupElem)
+   end
 
    println("PASS")
 end
 
 function test_perm_printing()
-   print("perm.printing...")
+   print(rpad("perm.printing...", 30))
 
-   R = PermutationGroup(10)
+   for T in types
+      print("$T ")
+      R = PermutationGroup(T(10))
 
-   b = R([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
+      b = R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
 
-   setpermstyle(:array);
-   @test string(b) == "[2, 3, 5, 4, 6, 7, 1, 9, 10, 8]"
+      setpermstyle(:array);
+      @test string(b) == "[2, 3, 5, 4, 6, 7, 1, 9, 10, 8]"
 
-   setpermstyle(:cycles);
-   @test string(b) == "(1,2,3,5,6,7)(8,9,10)"
+      setpermstyle(:cycles);
+      @test string(b) == "(1,2,3,5,6,7)(8,9,10)"
+   end
 
    println("PASS")
 end
 
 function test_perm_basic_manipulation()
-   print("perm.basic_manipulation...")
+   print(rpad("perm.basic_manipulation...", 30))
 
-   R = PermutationGroup(10)
+   for T in types
+      print("$T ")
+      R = PermutationGroup(T(10))
 
-   a = R()
-   b = deepcopy(a)
-   c = R([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
+      a = R()
+      b = deepcopy(a)
+      c = R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
 
-   @test a == b
+      @test a == b
 
-   @test parity(a) == 0
-   @test parity(c) == 1
+      @test parity(a) == 0
+      @test parity(c) == 1
 
-   @test length(unique([c,deepcopy(c)])) == 1
+      @test length(unique([c,deepcopy(c)])) == 1
 
-   a[1] = 5
-   @test a[1] == 5
+      a[1] = T(5)
+      @test a[1] == T(5)
+      @test setindex!(a, 5, 1) == T(5)
+
+   end
 
    println("PASS")
 end
 
 function test_perm_iteration()
-   print("perm.iteration...")
+   print(rpad("perm.iteration...", 30))
+   for T in types
+      print("$T ")
+      G = PermutationGroup(T(6))
+      @test length(AllPerms(T(6))) == 720
+      @test length(unique(AllPerms(T(6)))) == 720
+      @test_skip order(G) isa Int
+      @test order(G) == 720
 
-   G = PermutationGroup(5)
-   @test length(AllPerms(5)) == 120
-   @test length(unique(AllPerms(5))) == 120
+      @test collect(elements(G))[1] == G()
 
-   @test collect(elements(G))[1] == G()
+      @test length(collect(elements(G))) == 720
+      @test length(unique(elements(G))) == 720
 
-   @test length(collect(elements(G))) == 120
-   @test length(unique(elements(G))) == 120
-
-   G = PermutationGroup(Int8(5))
-   @test length(AllPerms(Int8(5))) == 120
-   @test length(unique(AllPerms(Int8(5)))) == 120
-
-   @test collect(elements(G))[1] == G()
-
-   @test length(collect(elements(G))) == 120
-   @test length(unique(elements(G))) == 120
-
-   G = PermutationGroup(Int8(6))
-   @test collect(elements(G)) isa Vector{Generic.perm{Int8}}
-   @test length(collect(elements(G))) == 720
-   @test Generic.AllPerms(G.n).all == 720
+   end
 
    println("PASS")
 end
 
 function test_perm_binary_ops()
-   print("perm.binary_ops...")
+   print(rpad("perm.binary_ops...", 30))
 
-   G = PermutationGroup(3)
+   for T in types
+      print("$T ")
+      G = PermutationGroup(T(3))
 
-   a = G([2,1,3])
-   b = G([2,3,1])
+      a = G(T[2,1,3])
+      b = G(T[2,3,1])
 
-   @test a*b == G([1,3,2])
-   @test b*a == G([3,2,1])
-   @test a*a == G()
-   @test b*b*b == G()
+      @test a*b == G(T[1,3,2])
+      @test b*a == G(T[3,2,1])
+      @test a*a == G()
+      @test b*b*b == G()
 
-   @test parity(G()) == 0
-   p = parity(a)
-   @test p == 1
-   cycles(a)
-   @test parity(a) == p
+      @test parity(G()) == 0
+      p = parity(a)
+      @test p == 1
+      cycles(a)
+      @test parity(a) == p
 
-   for a in elements(G), b in elements(G)
-      @test parity(a*b) == (parity(b)+parity(a)) % 2
+      for a in elements(G), b in elements(G)
+            @test parity(a*b) == (parity(b)+parity(a)) % 2
+      end
+
+      G = PermutationGroup(T(10))
+      p = G(T[9,5,4,7,3,8,2,10,1,6]) # (1,9)(2,5,3,4,7)(6,8,10)
+      @test p^0 == G()
+      @test p^1 == p
+      @test p^-1 == inv(p)
+      @test p^5 == p*p*p*p*p
+      @test p^-4 == inv(p)*inv(p)*inv(p)*inv(p)
+      @test p^2 * p^-2 == G()
+
    end
+   println("PASS")
+end
 
-   G = PermutationGroup(10)
-   p = G([9,5,4,7,3,8,2,10,1,6]) # (1,9)(2,5,3,4,7)(6,8,10)
-   @test p^0 == G()
-   @test p^1 == p
-   @test p^-1 == inv(p)
-   @test p^5 == p*p*p*p*p
-   @test p^-4 == inv(p)*inv(p)*inv(p)*inv(p)
-   @test p^2 * p^-2 == G()
    println("PASS")
 end
 
 function test_perm_inversion()
-   print("perm.inversion...")
+   print(rpad("perm.inversion...", 30))
+   for T in types
+      print("$T ")
+      R = PermutationGroup(T(10))
 
-   R = PermutationGroup(10)
+      a = R()
+      b = R(T[2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
 
-   a = R()
-   b = R([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
+      @test a == inv(a)
+      @test a == b*inv(b)
 
-   @test a == inv(a)
-   @test a == b*inv(b)
-
-   G = PermutationGroup(3)
-   for a in elements(G), b in elements(G)
-      @test inv(a*b) == inv(b)*inv(a)
+      G = PermutationGroup(3)
+      for a in elements(G), b in elements(G)
+         @test inv(a*b) == inv(b)*inv(a)
+      end
    end
-
    println("PASS")
 end
 
 function test_misc_functions()
-   print("perm.misc...")
+   print(rpad("perm.misc...", 30))
 
-   G = PermutationGroup(10)
-   a = G([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
+   for T in types
+      print("$T ")
+      G = PermutationGroup(T(10))
+      a = G([2, 3, 5, 4, 6, 7, 1, 9, 10, 8])
 
-   @test cycles(G()) == [[i] for i in 1:10]
-   @test order(G()) == 1
-   @test cycles(a) == [[1,2,3,5,6,7], [4], [8,9,10]]
-   @test order(a) == 6
-   @test a^6 == G()
+      @test cycles(G()) isa Generic.CycleDec{T}
+      @test collect(cycles(G())) == [T[i] for i in 1:10]
+      @test order(G()) isa Int
+      @test order(G()) == 1
+      @test collect(cycles(a)) == [T[1,2,3,5,6,7], T[4], T[8,9,10]]
+      @test cycles(a)[1] isa Vector{T}
+      @test cycles(a)[1] == T[1,2,3,5,6,7]
+      @test cycles(a)[2] == T[4]
+      @test cycles(a)[3] == T[8,9,10]
+      @test cycles(a)[1:3] == [T[1,2,3,5,6,7], T[4], T[8,9,10]]
 
-   p = G([9,5,4,7,3,8,2,10,1,6])
+      @test order(a) isa Int
+      @test order(a) == 6
+      @test a^6 == G()
 
-   @test cycles(p) == [[1,9],[2,5,3,4,7],[6,8,10]]
-   @test order(p) == 30
-   @test cycles(p^2) == [[1], [2,3,7,5,4], [6,10,8], [9]]
-   @test order(p^2) == 15
-   @test cycles(p^3) == [[1,9], [2,4,5,7,3], [6], [8], [10]]
-   @test order(p^3) == 10
-   @test cycles(p^5) == [[1,9], [2], [3], [4], [5], [6,10,8], [7]]
-   @test order(p^5) == 6
+      p = G([9,5,4,7,3,8,2,10,1,6])
 
-   M = matrix_repr(a)
-   for (idx, val) in enumerate(a.d)
-      @test M[idx, val] == 1
+      @test collect(cycles(p)) == [T[1,9],T[2,5,3,4,7],T[6,8,10]]
+      @test order(p) == 30
+      @test collect(cycles(p^2)) == [T[1], T[2,3,7,5,4], T[6,10,8], T[9]]
+      @test order(p^2) == 15
+      @test collect(cycles(p^3)) == [T[1,9], T[2,4,5,7,3], T[6], T[8], T[10]]
+      @test order(p^3) == 10
+      @test collect(cycles(p^5)) == [T[1,9], T[2], T[3], T[4], T[5], T[6,10,8], T[7]]
+      @test order(p^5) == 6
+
+      @test matrix_repr(a) isa Matrix{T}
+      M = matrix_repr(a)
+      for (idx, val) in enumerate(a.d)
+         @test M[idx, val] == 1
+      end
    end
 
    println("PASS")
 end
 
 function test_characters()
-   print("perm.characters...")
+   print(rpad("perm.characters...",30))
 
-   N = 8
+   for T in types
+   print("$T ")
+   N = T(7)
    G = PermutationGroup(N)
-   @test all(character(p)(G()) == dim(YoungTableau(p)) for p=AllParts(N))
+   @test all(character(p)(G()) == dim(YoungTableau(p)) for p in AllParts(N))
 
-   @test character(Partition([2,2,2,2]), Partition([8])) == 0
+   @test character(Partition(T[2,2,2,2]), Partition(T[8])) == 0
 
-   N = 3
+   N = T(3)
    G = PermutationGroup(N)
-   ps = Partition.([[1,1,1], [2,1], [3]])
-   l = Partition([1,1,1])
+   ps = Partition.([T[1,1,1], T[2,1], T[3]])
+   l = Partition(T[1,1,1])
    @test [character(l, m) for m in ps] == [ 1,-1, 1]
-   l = Partition([2,1])
+   l = Partition(T[2,1])
    @test [character(l, m) for m in ps] == [ 2, 0,-1]
-   l = Partition([3])
+   l = Partition(T[3])
    @test [character(l, m) for m in ps] == [ 1, 1, 1]
 
-   N = 4
+   N = T(4)
    G = PermutationGroup(N)
 
    ps = Partition.([[1,1,1,1], [2,1,1], [2,2], [3,1], [4]])
    @test Set(AllParts(N)) == Set(ps)
 
    l = Partition([1,1,1,1])
+
    @test [character(l, m) for m in ps] == [ 1,-1, 1, 1,-1]
-   l = Partition([2,1,1])
+   l = Partition(T[2,1,1])
    @test [character(l, m) for m in ps] == [ 3,-1,-1, 0, 1]
-   l = Partition([2,2])
+   l = Partition(T[2,2])
    @test [character(l, m) for m in ps] == [ 2, 0, 2,-1, 0]
-   l = Partition([3,1])
+   l = Partition(T[3,1])
    @test [character(l, m) for m in ps] == [ 3, 1,-1, 0,-1]
-   l = Partition([4])
+   l = Partition(T[4])
    @test [character(l, m) for m in ps] == [ 1, 1, 1, 1, 1]
 
    # values taken from GAP; note that we specify the order of partitions to be
    # compatible with GAP numbering of conjugacy classes. This is NOT the order
    # of partitions given by AllParts.
-   N = 5
+   N = T(5)
    G = PermutationGroup(N)
-   ps = Partition.([[1,1,1,1,1], [2,1,1,1], [2,2,1], [3,1,1], [3,2], [4,1], [5]])
-   l = Partition([1,1,1,1,1])
+   ps = Partition.([T[1,1,1,1,1], T[2,1,1,1], T[2,2,1], T[3,1,1], T[3,2], T[4,1], T[5]])
+   l = Partition(T[1,1,1,1,1])
    @test [character(l, m) for m in ps] == [   1,  -1,   1,   1,  -1,  -1,   1 ]
-   l = Partition([2,1,1,1])
+   l = Partition(T[2,1,1,1])
    @test [character(l, m) for m in ps] == [   4,  -2,   0,   1,   1,   0,  -1 ]
-   l = Partition([2,2,1])
+   l = Partition(T[2,2,1])
    @test [character(l, m) for m in ps] == [   5,  -1,   1,  -1,  -1,   1,   0 ]
-   l = Partition([3,1,1])
+   l = Partition(T[3,1,1])
    @test [character(l, m) for m in ps] == [   6,   0,  -2,   0,   0,   0,   1 ]
-   l = Partition([3,2])
+   l = Partition(T[3,2])
    @test [character(l, m) for m in ps] == [   5,   1,   1,  -1,   1,  -1,   0 ]
-   l = Partition([4,1])
+   l = Partition(T[4,1])
    @test [character(l, m) for m in ps] == [   4,   2,   0,   1,  -1,   0,  -1 ]
-   l = Partition([5])
+   l = Partition(T[5])
    @test [character(l, m) for m in ps] == [   1,   1,   1,   1,   1,   1,   1 ]
+   end
 
    # test for overflow
    p = Partition(collect(10:-1:1))
