36cb46a17b69a9df2a65da642d21b265e7710820
William Hart <goodwillhart@googlemail.com>
2018-04-23 17:00:15 +0200

    FunctionalCompositeMap and FunctionalMap tests.

--- test/generic/Map-test.jl
+++ test/generic/Map-test.jl
@@ -10,7 +10,7 @@ a(f::Map(MyMap)) = Generic.get_field(f, :a)
 (f::MyMap)(x) =  a(f)*(x + 1)
 
 function test_gen_map_functional()
-   print("Generic.Map.functional...")
+   print("Generic.Map.FunctionalMap...")
 
    f = map_from_func(ZZ, ZZ, x -> x + 1)
    g = map_from_func(ZZ, QQ, x -> QQ(x))
@@ -18,15 +18,49 @@ function test_gen_map_functional()
    @test isa(f, Map(FunctionalMap))
    @test isa(g, Map(FunctionalMap))
 
+   @test domain(f) == AbstractAlgebra.JuliaZZ
+   @test codomain(f) == AbstractAlgebra.JuliaZZ
+   @test domain(g) == AbstractAlgebra.JuliaZZ
+   @test codomain(g) == AbstractAlgebra.JuliaQQ
+
+   @test image_fn(f)(ZZ(1)) == 2
+   @test image_fn(g)(ZZ(2)) == QQ(2)
+
+   println("PASS")
+end
+
+function test_gen_map_functional_composite()
+   print("Generic.Map.FunctionalCompositeMap...")
+
+   f = map_from_func(ZZ, ZZ, x -> x + 1)
+   g = map_from_func(ZZ, QQ, x -> QQ(x))
+
    h = compose(f, g)
 
    @test isa(h, Map(FunctionalMap))
+   @test isa(h, Map(Generic.FunctionalCompositeMap))
 
    k = f*g
 
    for i in 1:10
       @test h(ZZ(i)) == k(ZZ(i))
    end
+
+   @test domain(h) == AbstractAlgebra.JuliaZZ
+   @test codomain(h) == AbstractAlgebra.JuliaQQ
+
+   @test image_fn(h)(ZZ(1)) == QQ(2)
+
+   @test map1(h) === f
+   @test map2(h) === g
+
+   println("PASS")
+end
+
+function test_gen_map_composite()
+   print("Generic.Map.CompositeMap...")
+
+   f = map_from_func(ZZ, ZZ, x -> x + 1)
 
    s = MyMap(2)
 
@@ -38,21 +72,19 @@ function test_gen_map_functional()
       @test t(ZZ(i)) == 2*(i + 2)
    end
 
-   @test domain(f) == AbstractAlgebra.JuliaZZ
-   @test codomain(f) == AbstractAlgebra.JuliaZZ
-   @test domain(g) == AbstractAlgebra.JuliaZZ
-   @test codomain(g) == AbstractAlgebra.JuliaQQ
    @test domain(t) == AbstractAlgebra.JuliaZZ
    @test codomain(t) == AbstractAlgebra.JuliaZZ
 
-   @test image_fn(f)(ZZ(1)) == 2
-   @test image_fn(g)(ZZ(2)) == QQ(2)
+   @test map1(t) === f
+   @test map2(t) === s
 
    println("PASS")
 end
 
 function test_gen_map()
    test_gen_map_functional()
+   test_gen_map_functional_composite()
+   test_gen_map_composite()
 
    println("")
 end
