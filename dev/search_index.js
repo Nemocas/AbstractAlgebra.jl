8e2a4bbd25cc691b4ac9871901168d69b401e994
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-28 09:24:35 +0000

    build based on c241fd5

--- dev/search_index.js
+++ dev/search_index.js
@@ -3565,7 +3565,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Generic matrices",
     "title": "AbstractAlgebra.Generic.fflu",
     "category": "method",
-    "text": "fflu(A::Generic.MatrixElem{T}, P = PermGroup(rows(A))) where {T <: RingElement}\n\nReturn a tuple r d p L U consisting of the rank of A, a denominator d, a permutation p of A belonging to P, a lower triangular matrix L and an upper triangular matrix U such that p(A) = LD^1U, where p(A) stands for the matrix whose rows are the given permutation p of the rows of A and such that D is the diagonal matrix diag(p_1 p_1p_2 ldots p_n-2p_n-1 p_n-1 where the p_i are the inverses of the diagonal entries of U. The denominator d is set to pm mboxdet(S) where S is an appropriate submatrix of A (S = A if A is square) and the sign is decided by the parity of the permutation.\n\n\n\n"
+    "text": "fflu(A::Generic.MatrixElem{T}, P = PermGroup(rows(A))) where {T <: RingElement}\n\nReturn a tuple r d p L U consisting of the rank of A, a denominator d, a permutation p of A belonging to P, a lower triangular matrix L and an upper triangular matrix U such that p(A) = LDU, where p(A) stands for the matrix whose rows are the given permutation p of the rows of A and such that D is the diagonal matrix diag(p_1 p_1p_2 ldots p_n-2p_n-1 p_n-1) where the p_i are the inverses of the diagonal entries of U. The denominator d is set to pm mboxdet(S) where S is an appropriate submatrix of A (S = A if A is square) and the sign is decided by the parity of the permutation.\n\n\n\n"
 },
 
 {
