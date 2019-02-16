9072c952341c6d1a12b373360ca8b3ad41a87333
Alex J. Best <alex.j.best@gmail.com>
2019-02-16 16:09:50 -0500

    dont treat -1 as special for series

--- src/generic/RelSeries.jl
+++ src/generic/RelSeries.jl
@@ -292,7 +292,7 @@ needs_parentheses(x::AbstractAlgebra.SeriesElem) = (!iszero(x))
 
 displayed_with_minus_in_front(x::AbstractAlgebra.SeriesElem) = false
 
-show_minus_one(::Type{AbstractAlgebra.SeriesElem{T}}) where {T <: RingElement} = show_minus_one(T)
+show_minus_one(::Type{AbstractAlgebra.SeriesElem{T}}) where {T <: RingElement} = false
 
 ###############################################################################
 #
