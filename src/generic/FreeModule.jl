5eaf370b50ee14cbe6acd51f82ef6cd6b0e3b276
William Hart <goodwillhart@googlemail.com>
2019-02-19 13:16:05 +0100

    Basic functionality for free module elements.

--- src/generic/FreeModule.jl
+++ src/generic/FreeModule.jl
@@ -22,6 +22,10 @@ elem_type(::Type{FreeModule{T}}) where T <: RingElement = free_module_elem{T}
 
 parent(m::free_module_elem{T}) where T <: RingElement = m.parent
 
+function check_parent(m1::free_module_elem{T}, m2::free_module_elem{T}) where T <: RingElement
+    parent(m1) != parent(m2) && ("Incompatible free modules")
+end
+
 function isdomain_type(::Type{free_module_elem{T}}) where T <: RingElement
    return isdomain_type(T)
 end
@@ -49,6 +53,74 @@ function show(io::IO, a::free_module_elem)
    print(io, "(")
    print(io, join(string.(a.v), ", "))
    print(io, ")")
+end
+
+###############################################################################
+#
+#   Unary operators
+#
+###############################################################################
+
+function -(m::free_module_elem)
+   M = parent(m)
+   return M(-m.v)
+end
+
+###############################################################################
+#
+#   Binary operators
+#
+###############################################################################
+
+function +(m1::free_module_elem{T}, m2::free_module_elem{T}) where T <: RingElement
+   check_parent(m1, m2)
+   M = parent(m1)
+   return M(m1.v + m2.v)
+end
+
+function -(m1::free_module_elem{T}, m2::free_module_elem{T}) where T <: RingElement
+   check_parent(m1, m2)
+   M = parent(m1)
+   return M(m1.v - m2.v)
+end
+
+###############################################################################
+#
+#   Ad hoc binary operators
+#
+###############################################################################
+
+function *(m::free_module_elem{T}, c::T) where T <: RingElement
+   parent(c) != base_ring(m) && error("Incompatible scalar")
+   M = parent(m)
+   return M([r*c for r in m.v])
+end
+
+function *(c::T, m::free_module_elem{T}) where T <: RingElement
+   parent(c) != base_ring(m) && error("Incompatible scalar")
+   M = parent(m)
+   return M([c*r for r in m.v])
+end
+
+function *(m::free_module_elem{T}, c::U) where {T <: RingElement, U <: Integer}
+   M = parent(m)
+   return M([r*c for r in m.v])
+end
+
+function *(c::U, m::free_module_elem{T}) where {T <: RingElement, U <: Integer}
+   M = parent(m)
+   return M([c*r for r in m.v])
+end
+
+###############################################################################
+#
+#   Comparison
+#
+###############################################################################
+
+function ==(m1::free_module_elem{T}, m2::free_module_elem{T}) where T <: RingElement
+   check_parent(m1, m2)
+   return m1.v == m2.v
 end
 
 ###############################################################################
--- test/generic/FreeModule-test.jl
+++ test/generic/FreeModule-test.jl
@@ -28,9 +28,54 @@ function test_free_module_manipulation()
    println("PASS")
 end
 
+function test_free_module_unary_ops()
+   print("Generic.FreeModule.unary_ops...")
+
+   R, x = PolynomialRing(ZZ, "x")
+   M = FreeModule(R, 3)
+
+   m = M([x, x, x])
+   
+   @test -m == M([-x, -x, -x])
+
+   println("PASS")
+end
+
+function test_free_module_binary_ops()
+   print("Generic.FreeModule.binary_ops...")
+
+   R, x = PolynomialRing(ZZ, "x")
+   M = FreeModule(R, 3)
+
+   m = M([x, x, x])
+
+   @test m + m - m == m
+
+   println("PASS")
+end
+
+function test_free_module_adhoc_binary()
+   print("Generic.FreeModule.adhoc_binary...")
+
+   R, x = PolynomialRing(ZZ, "x")
+   M = FreeModule(R, 3)
+
+   m = M([x, x, x])
+
+   @test 2*m == M([2x, 2x, 2x])
+   @test m*2 == 2*m
+   @test x*m == M([x^2, x^2, x^2])
+   @test m*x == x*m
+
+   println("PASS")
+end
+
 function test_free_module()
    test_free_module_constructors()
    test_free_module_manipulation()
+   test_free_module_unary_ops()
+   test_free_module_binary_ops()
+   test_free_module_adhoc_binary()
 
    println("")
 end
