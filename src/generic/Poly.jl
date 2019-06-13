16101864ec47f5a7cd77cb4df78124cec0e483a4
Simon Brandhorst <sbrandhorst@web.de>
2019-06-13 11:53:53 +0200

    fixes for change_base_ring

--- src/generic/Poly.jl
+++ src/generic/Poly.jl
@@ -40,14 +40,15 @@ base_ring(R::AbstractAlgebra.PolyRing{T}) where T <: RingElement = R.base_ring::
 base_ring(a::PolynomialElem) = base_ring(parent(a))
 
 @doc Markdown.doc"""
-    change_base_ring(::Poly{T}, g::any)
-> Return the polynomial obtained by applying `g` to the coefficients. The new base ring is defined by the image of `1`.
+    change_base_ring(::Poly{T}, g::Any)
+> Return the polynomial obtained by applying `g` to the coefficients. The new base ring is defined by the image of `0`.
 """
 function change_base_ring(p::Poly{T}, g) where T <: RingElement
-   new_base_ring = parent(g(zero(base_ring(p.parent))))
-   new_var_name = string(var(p.parent))
+   z = zero(base_ring(parent(p)))
+   new_base_ring = parent(g(z))
+   new_var_name = string(var(parent(p)))
    P, _ = PolynomialRing(new_base_ring, new_var_name)
-   new_coefficients = [g(c) for c in p.coeffs]
+   new_coefficients = [g(coeff(p,i)) for i in 0:degree(p)]
    return P(new_coefficients)
 end
 
