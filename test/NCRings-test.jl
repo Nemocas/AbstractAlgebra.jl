d23a0ab9591646d7f0cb836d7c28139907e28348
Tommy Hofmann <thofma@gmail.com>
2019-09-27 09:38:18 +0200

    Add Base.oftype

--- test/NCRings-test.jl
+++ test/NCRings-test.jl
@@ -1,2 +1,10 @@
 include("generic/MatrixAlgebra-test.jl")
 include("generic/NCPoly-test.jl")
+
+@testset "Rings.oftype..." begin
+   F = GF(3)
+   Fx, x = PolynomialRing(F, "x")
+   z = oftype(x, 3)
+   @test z == Fx(3)
+   @test parent(z) === Fx
+end
