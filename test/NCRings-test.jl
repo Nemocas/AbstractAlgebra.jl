88488e87c1e21259302c0a7c0515ec9a4a35bdd0
William Hart <goodwillhart@googlemail.com>
2019-10-08 15:07:21 +0200

    Rename some tests and move some to the correct place.

--- test/NCRings-test.jl
+++ test/NCRings-test.jl
@@ -1,7 +1,7 @@
 include("generic/MatrixAlgebra-test.jl")
 include("generic/NCPoly-test.jl")
 
-@testset "Rings.oftype..." begin
+@testset "NCRings.oftype..." begin
    F = GF(3)
    Fx, x = PolynomialRing(F, "x")
    z = oftype(x, 3)
