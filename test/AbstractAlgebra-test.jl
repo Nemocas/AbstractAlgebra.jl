f00a854c3928025ab7b3ce70c28def7d93522b6b
William Hart <goodwillhart@googlemail.com>
2019-10-08 14:38:47 +0200

    Move Maps-test back to its rightful place.

--- test/AbstractAlgebra-test.jl
+++ test/AbstractAlgebra-test.jl
@@ -2,11 +2,11 @@ using Random: MersenneTwister
 
 const rng = MersenneTwister()
 
-include("Maps-test.jl")
 include("Groups-test.jl")
 include("NCRings-test.jl")
 include("Rings-test.jl")
 include("Fields-test.jl")
 include("Modules-test.jl")
+include("Maps-test.jl")
 include("Benchmark-test.jl")
 
