599e483842fbd709ea306a7ca19e745a91156daf
Avi Kulkarni <avinash@mis.mpg.de>
2019-12-02 17:49:35 +0100

    Added more of the Hecke functions and Doc strings.

--- src/generic/Matrix-auf-Hecke.jl
+++ src/generic/Matrix-auf-Hecke.jl
@@ -47,7 +47,7 @@ must be non-empty.
 """
 function matrix(A::Array{T, 2}) where T <: RingElem
   r, c = size(A)
-  (r <= 0 || c <= 0) && throw(DomainError("Array must be non-empty"))
+  isempty(A) && throw(DomainError("$A. Array must be non-empty"))
   m = matrix(parent(A[1, 1]), A)
   return m
 end
@@ -61,6 +61,10 @@ function matrix(A::Array{T, 1}) where T <: RingElem
   return matrix(reshape(A,length(A),1))
 end
 
+@doc Markdown.doc"""
+    scalar_matrix(R::Ring, n::Int, a::RingElement)
+Construct the AbstractAlgebra matrix `a*I`, with `I` the `n x n` identity matrix.
+"""
 function scalar_matrix(R::Ring, n::Int, a::RingElement)
   b = R(a)
   z = zero_matrix(R, n, n)
@@ -68,5 +72,68 @@ function scalar_matrix(R::Ring, n::Int, a::RingElement)
     z[i, i] = b
   end
   return z
+end
+
+################################################################################
+#
+#  Zero checking.
+#
+################################################################################
+
+@doc Markdown.doc"""
+    iszero_row(M::MatElem{T}, i::Int) where T
+Check if the `i`-th row of the matrix `M` is zero. Note this function is much
+faster than either `iszero(M[1,:])` or `iszero(A.entries[i,:])`.
+"""
+
+function iszero_row(M::Union{MatElem{T}, Array{T,2}}, i::Int) where T
+  for j in 1:ncols(M)
+    if !iszero(M[i,j])
+      return false
+    end
+  end
+  return true
+end
+
+
+################################################################################
+#
+#  Diagonal (block) matrix creation
+#
+################################################################################
+
+@doc Markdown.doc"""
+    diagonal_matrix(x::Vector{T}) where T <: NCRingElem -> MatElem{T}
+    diagonal_matrix(x::T...) where T <: NCRingElem -> MatElem{T}
+Returns a diagonal matrix whose diagonal entries are the element of `x`.
+"""
+function diagonal_matrix(x::Vector{T}) where T <: NCRingElem
+  isempty(x) && throw(DomainError("$x. Array must be non-empty."))
+  M = zero_matrix(parent(x[1]), length(x), length(x))
+  for i = 1:length(x)
+    M[i, i] = x[i]
+  end
+  return M
+end
+
+function diagonal_matrix(x::T...) where T <: NCRingElem
+  return diagonal_matrix(collect(x))
+end
+
+@doc Markdown.doc"""
+    diagonal_matrix(x::Vector{T}) where T <: MatElem -> MatElem
+    diagonal_matrix(x::T...) where T <: MatElem -> MatElem
+
+Returns a block diagonal matrix whose diagonal blocks are the matrices in $x$.
+"""
+function diagonal_matrix(x::Vector{T}) where T <: MatElem
+  return diagonal_matrix(x...)
+end
+
+function diagonal_matrix(x::T...) where T <: MatElem
+    (isempty(x) || all([isempty(M) for M in x])) && (
+        throw(DomainError("$x. Not all matrices can be empty.")))
+    
+    return cat(x..., dims = (1, 2))
 end
 
