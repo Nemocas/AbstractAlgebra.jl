72e88b88ecd65af37e7b38f3282892c461e2d7ad
Avi Kulkarni <avinash@mis.mpg.de>
2019-12-03 11:22:31 +0100

    Added some tests.

--- test/generic/Matrix-Hecke-test.jl
+++ test/generic/Matrix-Hecke-test.jl
@@ -9,4 +9,15 @@
 
     @test_throws DomainError matrix(fill(one(R), 0, 5))
 
+    @test_throws DomainError diagonal_matrix(fill(one(R),0))
+
+    @test isdiagonal(diagonal_matrix(fill(one(R), 5)))
+
+    @test isdiagonal(diagonal_matrix(fill(zero(R), 5)))
+
+    @test isdiagonal(matrix(fill(one(R), 5, 5))) == false
+
+    @test isdiagonal(matrix(R,0,0,fill(one(R),0,0)))
+
+    
 end
