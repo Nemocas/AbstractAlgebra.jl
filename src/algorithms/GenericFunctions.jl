8cd3c67a2085afc1875df6882645e708de8527d0
Albin Ahlbäck <albin.ahlback@gmail.com>
2021-03-19 18:02:15 +0100

    Comment out docstring in GenericFunctions.jl

--- src/algorithms/GenericFunctions.jl
+++ src/algorithms/GenericFunctions.jl
@@ -7,25 +7,25 @@
 # TODO: Move from CRT from Hecke to AbstractAlgebra?
 # Currently no implementation, only example on how the arbitrary inputs `crt`
 # should look like.
-@doc Markdown.doc"""
-    crt(r::AbstractVector{T}, m::AbstractVector{T}) where T
-    crt(r::T, m::T...) where T
+# @doc Markdown.doc"""
+#     crt(r::AbstractVector{T}, m::AbstractVector{T}) where T
+#     crt(r::T, m::T...) where T
 
-Return $x$ in the Euclidean domain $T$ such that $x \equiv r_i \mod m_i$
-for all $i$.
-"""
+# Return $x$ in the Euclidean domain $T$ such that $x \equiv r_i \mod m_i$
+# for all $i$.
+# """
 function crt end
 
-@doc Markdown.doc"""
-    factor(a::T, b::R)
+# @doc Markdown.doc"""
+#     factor(a::T, b::R)
 
-Return factorization of element $a$. 
-"""
+# Return factorization of element $a$. 
+# """
 function factor end
 
-@doc Markdown.doc"""
-    factor_squarefree(a::T)
+# @doc Markdown.doc"""
+#     factor_squarefree(a::T)
 
-Return square free factorization of element $a$.
-"""
+# Return square free factorization of element $a$.
+# """
 function factor_squarefree end
