661411543e0683c690f16b06b88bba525bb0f93c
William Hart <goodwillhart@googlemail.com>
2021-09-01 15:03:59 +0200

    Set up basic nodes, heap and tree and deal with constant polys.

--- src/generic/Ideal.jl
+++ src/generic/Ideal.jl
@@ -33,18 +33,157 @@ gens(I::Ideal) = I.gens
 
 ###############################################################################
 #
-#   Ideal reduction
+#   Ideal reduction for mulivariates over Euclidean domain
 #
 ###############################################################################
 
-function reduce_euclidean(I::Ideal{T}) where T <: RingElement
-end
+mutable struct lmnode{U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+   poly::U
+   up::Union{lmnode{U}, Nothing}   # out (divisible leading monomials)
+   next::Union{lmnode{U}, Nothing} # extend current node so out-degree can be > 1
+   lm::NTuple{N, Int}   # leading monomial as exponent vector
+   lcm::NTuple{N, Int}  # lcm of lm's in tree rooted here, as exponent vector
+   in_heap::Bool # whether node is in the heap
 
-function reduce(I::Ideal{T}) where T <: RingElement
-   if hasmethod(gcdx, Tuple{T, T})
-      I = reduce_euclidean(I)
+   function lmnode{U, N}(p::U) where {U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+      node = new{U, N}(p, nothing, nothing)
+      node.lm = Tuple(exponent_vector(p, 1))
+      node.lcm = node.lm
+      return node::lmnode{U, N}
    end
 end
+
+# heap implementation for sorting polys by lm, head = smallest
+
+# Defined in MPoly, repeated here for reference
+# heapleft(i::Int) = 2i
+# heapright(i::Int) = 2i + 1
+# heapparent(i::Int) = div(i, 2)
+
+function lm_precedes(node1::lmnode{U, N}, node2::lmnode{U, N}) where {U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+   for i = 1:N
+      if node2.lm[i] > node1.lm[i]
+         return false
+      end
+   end
+   return true
+end
+
+function lm_lcm(node1::lmnode{U, N}, node2::lmnode{U, N}) where {U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+   return Tuple(max(node1.lcm[i], node2.lcm[i]) for i in 1:N)
+end
+
+function heapinsert!(heap::Vector{lmnode{U, N}}, node::lmnode{U, N}) where {U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+   i = n = length(heap) + 1
+   if i != 1 && lm_precedes(heap[1], node)
+      i = 1
+   else
+      @inbounds while (j = heapparent(i)) >= 1
+         if lm_precedes(heap[j], node)
+            i = j
+         else
+            break
+         end
+      end
+   end
+   push!(heap, node)
+   while n > i
+      heap[n] = heap[heapparent(n)]
+      n >>= 1
+   end
+   heap[i] = node
+   node.in_heap = true
+   return Nothing
+end
+
+function heappop!(heap::Vector{lmnode{U, N}}) where {U <: AbstractAlgebra.MPolyElem{<:RingElement}, N}
+   s = length(heap)
+   x = heap[1]
+   i = 1
+   j = 2
+   @inbounds while j < s
+      if !lm_precedes(heap[j + 1], heap[j])
+         j += 1
+      end
+      heap[i] = heap[j]
+      i = j
+      j <<= 1
+   end
+   n = heap[s]
+   j = i >> 1
+   @inbounds while i > 1 && lm_precedes(heap[j], n)
+      heap[i] = heap[j]
+      i = j
+      j >>= 1
+   end
+   heap[i] = heap[s]
+   pop!(heap)
+   x.in_heap = false
+   return x
+end
+
+function extract_gens(V::Vector{U}, node::T) where {N, U <: AbstractAlgebra.MPolyElem, T <: lmnode{U, N}}
+   node.in_heap = true # mark node as visited
+   # depth first
+   if node.up != nothing
+      V = extract_gens(V, node.up)
+   end
+   if node.next != nothing
+      V = extract_gens(V, node.next)
+   end
+   push!(V, node.poly)
+   return V
+end
+
+function extract_gens(B::Vector{T}) where {N, U <: AbstractAlgebra.MPolyElem, T <: lmnode{U, N}}
+   V = Vector{U}()
+   for node in B
+      V = extract_gens(V, node)
+   end
+   return V
+end
+
+function reduce(I::Ideal{U}) where {T <: RingElement, U <: AbstractAlgebra.MPolyElem{T}}
+   if hasmethod(gcdx, Tuple{T, T})
+      V = gens(I)
+      # Step 1: compute V a vector of polynomials giving the same basis as I
+      #         but for which 1, 2, 3 above hold
+      if length(V) > 1
+         # make heap
+         N = nvars(base_ring(I))
+         heap = Vector{lmnode{U, N}}()
+         for v in V
+            heapinsert!(heap, lmnode{U, N}(v))
+         end
+         d = heappop!(heap)
+         # take gcd of constant polys
+         if isconstant(d.poly)
+            S = parent(d.poly)
+            d0 = constant_coefficient(d.poly)
+            while !isempty(heap) && isconstant(heap[1].poly)
+               di = constant_coefficient(pop!(heap).poly)
+               d0 = gcd(d0, di)
+            end
+            d.poly = S(d0)
+         end
+         # canonicalise
+         d.poly = divexact(d.poly, canonical_unit(d.poly))
+         B = [d]
+         # do reduction
+         # extract polynomials from B
+         V = extract_gens(B)
+      end
+      return Ideal{U}(base_ring(I), V)
+   else
+      error("Not implemented")
+   end
+end
+
+###############################################################################
+#
+#   Ideal reduction for univariates over Euclidean domain
+#
+###############################################################################
 
 # Extend the basis V of polynomials satisfying 1-6 below by the
 # polynomials in D, all of which have degree at least that of those in
@@ -182,6 +321,21 @@ function reduce(I::Ideal{T}) where {U <: RingElement, T <: AbstractAlgebra.PolyE
       return Ideal{T}(base_ring(I), V)
    else
       error("Not implemented")
+   end
+end
+
+###############################################################################
+#
+#   Ideal reduction in Euclidean domain
+#
+###############################################################################
+
+function reduce_euclidean(I::Ideal{T}) where T <: RingElement
+end
+
+function reduce(I::Ideal{T}) where T <: RingElement
+   if hasmethod(gcdx, Tuple{T, T})
+      I = reduce_euclidean(I)
    end
 end
 
