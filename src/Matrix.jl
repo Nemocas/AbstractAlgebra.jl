18e2fc76f6778cbd65743d50e2d11e9e734b7964
Max Horn <max@quendi.de>
2022-05-07 00:16:57 +0200

    add a missing space in docstring

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -302,7 +302,7 @@ function block_diagonal_matrix(V::Vector{<:MatElem{T}}) where T <: NCRingElement
 end
 
 @doc Markdown.doc"""
-   block_diagonal_matrix(R::NCRing, V::Vector{<:Matrix{T}}) where T <: NCRingElement
+    block_diagonal_matrix(R::NCRing, V::Vector{<:Matrix{T}}) where T <: NCRingElement
 
 Create the block diagonal matrix over the ring `R` whose blocks are given
 by the matrices in `V`. Entries are coerced into `R` upon creation.
