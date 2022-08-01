6d4fa2041000bcfce21620204eff47ddbe6847b0
Tommy Hofmann <thofma@gmail.com>
2022-08-01 20:09:31 +0200

    Fix mulmod and crt for fixed precision (#1204)

--- src/algorithms/GenericFunctions.jl
+++ src/algorithms/GenericFunctions.jl
@@ -85,6 +85,10 @@ function mulmod(a::T, b::T, m::T) where T <: RingElement
    return mod(a*b, m)
 end
 
+function mulmod(a::T, b::T, m::T) where T <: Integer
+   return mod(widen(a)*b, m) % T
+end
+
 function internal_powermod(a, n, m)
    @assert n > 1
    while iseven(n)
@@ -337,6 +341,9 @@ end
 
 Return an element congruent to $r_1$ modulo $m_1$ and $r_2$ modulo $m_2$.
 If `check = true` and no solution exists, an error is thrown.
+
+If `T` is a fixed precision integer type (like `Int`), the result will be
+correct if `abs(ri) <= abs(mi)` and `abs(m1 * m2) < typemax(T)`.
 """
 function crt(r1::T, m1::T, r2::T, m2::T; check::Bool=true) where T <: RingElement
    return _crt_stub(r1, m1, r2, m2; check=check)
