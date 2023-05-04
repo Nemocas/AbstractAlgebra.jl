615827ec12eb5897714458d2546e4e00a8e22478
Max Horn <max@quendi.de>
2023-05-04 17:37:53 +0200

    Fix ResidueRing / ResidueField deprecation message

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -3,13 +3,13 @@
 # we can't use @deprecate because this is also a typename
 function ResidueRing(R::Ring, a::RingElement; cached::Bool = true)
     Base.depwarn("'ResidueRing(R::Ring, a::RingElement; cached::Bool = true)' is deprecated, use "*
-    "'residue_ring(C::Cone; set_attributes::Bool = true)' instead.", :ResidueRing)
+    "'residue_ring(R::Ring, a::RingElement; cached::Bool = true)' instead.", :ResidueRing)
     return residue_ring(R, a; cached)
 end
 
 # we can't use @deprecate because this is also a typename
 function ResidueField(R::Ring, a::RingElement; cached::Bool = true)
     Base.depwarn("'ResidueField(R::Ring, a::RingElement; cached::Bool = true)' is deprecated, use "*
-    "'residue_field(C::Cone; set_attributes::Bool = true)' instead.", :ResidueField)
+    "'residue_field(R::Ring, a::RingElement; cached::Bool = true)' instead.", :ResidueField)
     return residue_field(R, a; cached)
 end
