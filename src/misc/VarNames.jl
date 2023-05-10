52d930a4ed53a029cff4beaf485903a5c369a0b6
Markus Kurtz <kurtz@mathematik.uni-kl.de>
2023-05-10 22:23:28 +0200

    Reorganize

--- src/misc/VarNames.jl
+++ src/misc/VarNames.jl
@@ -1,13 +1,13 @@
 using Base.Iterators
 
-const VarName = Union{Symbol, AbstractString, Char}
-const VarShape = Union{Missing, Int, Tuple{Vararg{Int}}}
-# const VarNamesVector = Vector{Union{<:VarName, Pair{<:VarName, <:VarShape}}}
+# const VarName = Union{Symbol, AbstractString, Char}
+# const VarShape = Union{Missing, Int, Tuple{Vararg{Int}}}
 # const VarNames = Union{
 #     Pair{<:VarName, <:VarShape},
 #     Dict{<:VarName, <:VarShape},
 #     NamedTuple{<:Any, Tuple{Vararg{VarShape}}},
 #     VarNamesVector
+#     Vector{Union{<:VarName, Pair{<:VarName, <:VarShape}}}
 # }
 
 """
@@ -19,46 +19,46 @@ Create proper variable names from `a`.
 
 ```jldoctest
 julia> variable_names([:x, :y])
-2-element Vector{Symbol}:
- :x
- :y
+2-element Vector{String}:
+ "x"
+ "y"
 
 julia> variable_names(:x => 3)
-3-element Vector{Symbol}:
- Symbol("x[1]")
- Symbol("x[2]")
- Symbol("x[3]")
+3-element Vector{String}:
+ "x[1]"
+ "x[2]"
+ "x[3]"
 
 julia> x = variable_names(:x => (3, 2))
-6-element Vector{Symbol}:
- Symbol("x[1,1]")
- Symbol("x[2,1]")
- Symbol("x[3,1]")
- Symbol("x[1,2]")
- Symbol("x[2,2]")
- Symbol("x[3,2]")
+6-element Vector{String}:
+ "x[1,1]"
+ "x[2,1]"
+ "x[3,1]"
+ "x[1,2]"
+ "x[2,2]"
+ "x[3,2]"
 
 julia> x == variable_names(:x => [3, 2])
 true
 
 julia> vars = variable_names((x = (1, 2), y = 2, z = missing))
-5-element Vector{Symbol}:
- Symbol("x[1,1]")
- Symbol("x[1,2]")
- Symbol("y[1]")
- Symbol("y[2]")
- :z
+5-element Vector{String}:
+ "x[1,1]"
+ "x[1,2]"
+ "y[1]"
+ "y[2]"
+ "z"
 
 julia> vars == variable_names([:x => (1, 2), :y => 2, :z])
 true
 
 julia> variable_names(Dict(:x => (1, 2), :y => 2, :z => missing))
-5-element Vector{Symbol}:
- Symbol("y[1]")
- Symbol("y[2]")
- :z
- Symbol("x[1,1]")
- Symbol("x[1,2]")
+5-element Vector{String}:
+ "y[1]"
+ "y[2]"
+ "z"
+ "x[1,1]"
+ "x[1,2]"
 ```
 """
 variable_names(a) = Symbol[Symbol(s, suffix) for (s, shape) in _pairs(a) for suffix in _variable_suffix(shape)]::Vector{Symbol}
@@ -93,10 +93,11 @@ It holds `reshape_to_varnames(a, variable_names(a)) == a`.
 # Examples
 
 ```jldoctest
-julia> R, vec = polynomial_ring(ZZ, variable_names((x=2, y=(3,2))));
+julia> R, vec = polynomial_ring(ZZ, variable_names((x=(1,2), y=2, z=missing)))
+(Multivariate Polynomial Ring in x[1,1], x[1,2], y[1], y[2], z over Integers, AbstractAlgebra.Generic.MPoly{BigInt}[x[1,1], x[1,2], y[1], y[2], z])
 
-julia> reshape_to_varnames(vec, (x=2, y=(1,2)))
-(x = [x[1], x[2]], y = [y[1,1] y[1,2]])
+julia> x, y, z = reshape_to_varnames(vec, (x=(1,2), y=2, z=missing))
+(x = AbstractAlgebra.Generic.MPoly{BigInt}[x[1,1] x[1,2]], y = AbstractAlgebra.Generic.MPoly{BigInt}[y[1], y[2]], z = z)
 """
 function reshape_to_varnames(vec::Vector, varnames)
     iter = Iterators.Stateful(vec)
@@ -126,72 +127,79 @@ __reshape(iter, s::VarName) = popfirst!(iter)
 __reshape(iter, (_, a)::Pair) = _reshape(iter, a)
 
 """
-    @with_variable_names f(..., s::Vector{Symbol}, ...)
+    @add_varnames_interface f(args..., varnames)
 
-Add method `f(..., s, ...)` which calls the underlying method after
-transforming `s` via a call to `variable_names`.
+Add method `X, vars = f(args..., varnames)` and macro `X = @f args... varnames...` to current scope. Read on.
 
-Format for `@f` is `@f(..., (x[1,2], y[2], z), ...)` or `@f(..., x[3,2], ...)`.
-Calling `@f(..., x, ...)` expects `f` to return a single generator.
+---
+
+    X, gens::Vector{T} = f(args..., vars::Vector{Symbol})
+
+This underlying method is assumed to exist. The existence is currently not tested.
+
+---
+
+    X, vars = f(args..., varnames)
+
+Compute `X` and `gens` by the underlying `f` method. Then reshape `gens` into the shape defined by `varnames`.
+
+---
+
+    X = @f args... varnames...
+    X = @f args... (varnames...)
+
+Compute `X` and `gens` by the underlying `f` method. Then introduce the `varnames` into the current scope.
 
 # Examples
 
-# as if :D
 ```jldoctest
-julia> f(a, s::Union{Vector{Symbol}, Symbol}) = a, s
+julia> f(a, s::Vector{Symbol}) = a, String.(s)
 f (generic function with 1 method)
 
-julia> @with_variable_names f(a, s::Vector{Symbol})
-@f (macro with 2 methods)
+julia> @add_varnames_interface f(a, s::Vector{Symbol})
+@f (macro with 1 method)
 
 julia> f
 f (generic function with 2 methods)
 
 julia> f("hello", (x = (1, 2), y = 2, z = missing))
-("hello", (x = [Symbol("x[1,1]") Symbol("x[1,2]")], y = [Symbol("y[1]"), Symbol("y[2]")], z = :z))
+("hello", (x = ["x[1,1]" "x[1,2]"], y = ["y[1]", "y[2]"], z = "z"))
 
-julia> @f "hello" (x = (1, 2), y = 2, z = missing)
+julia> @f "hello" x[1, 2] y[2] z
 "hello"
 
 julia> x
-1×2 Matrix{Symbol}:
- Symbol("x[1,1]")
- Symbol("x[1,2]")
+1×2 Matrix{String}:
+ "x[1,1]"  "x[1,2]"
 
 julia> y
-2-element Vector{Symbol}:
- Symbol("y[1]")
- Symbol("y[2]")
+2-element Vector{String}:
+ "y[1]"
+ "y[2]"
 
 julia> z
- :z
+ "z"
 ```
 """
-
-macro with_variable_names(e::Expr)
+macro add_varnames_interface(e::Expr)
     @assert Base.isexpr(e, :call)
     f = esc(e.args[1])
-    args0, _, args1 = _splice(_is_symbol_vector, e.args[2:end])
-    s = gensym("varnames") # `:s` could appear in `args0`
+    args = e.args[2:end-1]
+    s = gensym("varnames")
+    kv = gensym("kv")
+    # TODO cope with `where` clause and keyvalues
     return quote
-        function $f($(args0...), $s, $(args1...))
-            X, gens = $f($(args0...), variable_names($s), $(args1...))
+        # TODO assert existence of underlying method
+        function $f($(args...), $s; $kv...)
+            X, gens = $f($(args...), variable_names($s); $kv...)
             return X, reshape_to_varnames(gens, $s)
         end
 
-        macro $f($(args0...), $s::Symbol, $(args1...))
-            quote
-                X, gen = $$f($$(args0...), $(QuoteNode($s)), $$(args1...))
-                $(esc($s)) = gen
-                X
-            end
-        end
-
-        macro $f($(args0...), $s::Expr, $(args1...))
+        macro $f($(args...), $s...)
             xs = _expr_pairs($s)
             varnames = Expr(:tuple, (:($k = $v) for (k, v) in xs)...)
             return Expr(:block,
-                :((X, gens) = $$f($$(args0...), $varnames, $$(args1...))),
+                :((X, gens) = $$f($$(args...), $varnames)),
                 (:($(esc(x)) = gens.$x) for (x, _) in xs)...,
                 :(X)
             )
@@ -199,19 +207,71 @@ macro with_variable_names(e::Expr)
     end
 end
 
-function _expr_pairs(e::Expr)
-    Base.isexpr(e, :ref) && (e = Expr(:tuple, e))
-    @assert Base.isexpr(e, :tuple)
-    return _expr_pair.(e.args)
-end
-_expr_pair(e::Expr) = (@assert Base.isexpr(e, :ref); e.args[1] => Tuple{Vararg{Int}}(e.args[2:end]))
+_expr_pairs(a::Tuple{Vararg{Union{Expr, Symbol}}}) = _expr_pair.(a)
+# for `@f args... (varnames...)` variant:
+_expr_pairs((a,)::Tuple{Expr}) = Base.isexpr(a, :tuple) ? _expr_pair.(a.args) : (_expr_pair(a),)
+_expr_pair(e::Expr) = (@assert Base.isexpr(e, :ref) "$(e.head) ≠ :ref"; e.args[1] => Tuple{Vararg{Int}}(e.args[2:end]))
 _expr_pair(s::Symbol) = s => missing
 
+"""
+    @add_varname_interface f(args0..., varname::VarName, args1...)
 
-_is_symbol_vector(a) = Base.isexpr(a, :(::), 2) && a.args[2] == :(Vector{Symbol})
+Add method `X, vars = f(args0..., varname::VarName, args1...)` and macro `X = @f args0... varname::Symbol, args1...` to current scope. Read on.
 
-function _splice(f::Function, v::Vector)
-    k = findfirst(f, v)
-    @assert k !== nothing "@with_variable_names called on function without `Vector{Symbol}`"
-    return v[1:k-1], v[k], v[k+1:end]
+---
+
+    X, gen::T = f(args0..., var::Symbol, args1...)
+
+This underlying method is assumed to exist. The existence is currently not tested.
+
+---
+
+    X, gen = f(args0..., varname::VarName, args1...) = f(args0..., Symbol(varname), args1...)
+
+---
+
+    X = @f args0... varname::Symbol args1...
+
+Compute `X` and `gen` by the underlying `f` method. Then introduce `varname = gen` into the current scope.
+
+# Examples
+
+```jldoctest
+julia> f(a, s::Symbol) = a, s
+f (generic function with 1 method)
+
+julia> @add_varname_interface f(a, s::Vector{Symbol})
+@f (macro with 2 methods)
+
+julia> f
+f (generic function with 2 methods)
+
+julia> f("hello", "x")
+("hello", :x)
+
+julia> @f "hello" x
+"hello"
+
+julia> x
+:x
+```
+"""
+macro add_varname_interface(e::Expr)
+    @assert Base.isexpr(e, :call)
+    f = esc(e.args[1])
+    args = e.args[2:end-1]
+    s = gensym("varnames")
+    kv = gensym("kv")
+    return quote
+        $f($(args...), $s::Union{AbstractString, Char}; $kv...) =
+            $f($(args...), Symbol($s); $kv...)
+
+        macro $f($(args...), $s::Symbol)
+            quote
+                X, gen = $$f($$(args...), $(QuoteNode($s)))
+                $(esc($s)) = gen
+                X
+            end
+        end
+    end
 end
