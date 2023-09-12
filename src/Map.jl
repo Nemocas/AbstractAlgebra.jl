7812899389563929ac474eb189b0426d2c9bb287
Max Horn <max@quendi.de>
2023-09-13 00:33:50 +0200

    Add docstrings for various map construction functions
    Also include jldoctests for each, and fix a bug in the
    printing of IdentityMap instances.

--- src/Map.jl
+++ src/Map.jl
@@ -27,6 +27,23 @@ end
 #
 ###############################################################################
 
+@doc raw"""
+    compose(f::Map, g::Map)
+
+Compose the two maps $f$ and $g$, i.e. return the map $h$ such that $h(x) = g(f(x))$.
+
+# Examples
+```jldoctest; setup = :(using AbstractAlgebra)
+julia> f = map_from_func(x -> x + 1, ZZ, ZZ);
+
+julia> g = map_from_func(x -> QQ(x), ZZ, QQ);
+
+julia> h = compose(f, g)
+Functional composite map
+  first map: integers -> integers
+  next map: integers -> rationals
+```
+"""
 function compose(f::Map, g::Map)
    check_composable(f, g)
    return Generic.CompositeMap(f, g)
@@ -40,6 +57,24 @@ end
 #
 ################################################################################
 
+@doc raw"""
+    identity_map(R::D) where D <: AbstractAlgebra.Set
+
+Return an identity map on the domain $R$.
+
+# Examples
+```jldoctest; setup = :(using AbstractAlgebra)
+julia> R, t = ZZ[:t]
+(Univariate polynomial ring in t over integers, t)
+
+julia> f = identity_map(R)
+Identity map
+  of univariate polynomial ring in t over integers
+
+julia> f(t)
+t
+```
+"""
 identity_map(R::D) where D <: AbstractAlgebra.Set = Generic.IdentityMap{D}(R)
 
 ################################################################################
@@ -48,6 +83,23 @@ identity_map(R::D) where D <: AbstractAlgebra.Set = Generic.IdentityMap{D}(R)
 #
 ################################################################################
 
+@doc raw"""
+    map_from_func(image_fn::Function, domain, codomain)
+
+Construct the generic functional map with domain and codomain given by the parent objects
+$R$ and $S$ corresponding to the Julia function $f$.
+
+# Examples
+```jldoctest; setup = :(using AbstractAlgebra)
+julia> f = map_from_func(x -> x + 1, ZZ, ZZ)
+Map defined by a Julia function
+  from integers
+  to integers
+
+julia> f(ZZ(2))
+3
+```
+"""
 function map_from_func(image_fn::Function, domain, codomain)
    return Generic.FunctionalMap(domain, codomain, image_fn)
 end
