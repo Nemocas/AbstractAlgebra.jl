c6206bab0b4e2b4564c64d9828677bf104e4621d
Max Horn <max@quendi.de>
2023-12-09 13:06:51 +0100

    Add generic is_irreducible, is_squarefree methods
    Also clarify their docstrings and those of factor and
    factor_squarefree; and use these docstrings in the manual.

--- src/Factor.jl
+++ src/Factor.jl
@@ -15,6 +15,8 @@
 
 Type for factored ring elements. The structure holds a unit of type `T` and is
 an iterable collection of `T => Int` pairs for the factors and exponents.
+
+See [`unit(a::Fac)`](@ref), [`evaluate(a::Fac)`](@ref).
 """
 mutable struct Fac{T <: RingElement}
    unit::T
