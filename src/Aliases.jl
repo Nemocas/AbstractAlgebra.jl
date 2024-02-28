5ea280d4d145c941c873cc42bb8dc3b0ca373206
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-02-28 10:36:18 +0100

    Change deprecation to alias

---
+++ src/Aliases.jl
@@ -0,0 +1,4 @@
+# to be deprecated eventually
+
+# added during 0.40.*
+@alias UniversalPolynomialRing universal_polynomial_ring
--- src/Deprecations.jl
+++
@@ -1,3 +0,0 @@
-# deprecated during 0.40.*
-
-@deprecate UniversalPolynomialRing universal_polynomial_ring
