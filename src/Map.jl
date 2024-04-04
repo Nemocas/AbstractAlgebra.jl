f0fa4df9b080aa8fa37c94eea542ab9633f829f9
Johannes Schmitt <jschmitt@posteo.eu>
2024-04-04 13:56:04 +0200

    Clean up

--- src/Map.jl
+++ src/Map.jl
@@ -18,8 +18,7 @@ function codomain end
 function image_fn end
 
 function check_composable(a::Map, b::Map)
-   return codomain(a) !== domain(b) && error("Incompatible maps")
-   codomain(a) != domain(b) && error("Incompatible maps")
+   codomain(a) !== domain(b) && error("Incompatible maps")
 end
 
 function Base.show(io::IO, ::MIME"text/plain", M::AbstractAlgebra.Map)
