795382d08dd4f0d798960146f33c4c82a84afd3b
Max Horn <max@quendi.de>
2024-04-18 02:55:08 +0200

    Fix `_is_attribute_storing_type` (#1669)

--- test/Attributes-test.jl
+++ test/Attributes-test.jl
@@ -49,6 +49,21 @@ end
 @attributes Tmp.Quux
 @attributes Tmp.FooBar{Tmp.Quux}
 
+@testset "_is_attribute_storing_type" begin
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Foo)
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Bar)
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Quux)
+
+  @test !AbstractAlgebra._is_attribute_storing_type(Tmp.FooBar)
+  @test !AbstractAlgebra._is_attribute_storing_type(Tmp.FooBar{Tmp.Foo})
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.FooBar{Tmp.Bar})
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.FooBar{Tmp.Quux})
+
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Container)
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Container{Tmp.Bar})
+  @test AbstractAlgebra._is_attribute_storing_type(Tmp.Container{Tmp.Quux})
+end
+
 # applying @attributes to a singleton typename is supported but does nothing
 @attributes Tmp.Singleton
 
