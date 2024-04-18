132d5161e085ce4ab24a3f2555f5ac4d55326021
Max Horn <max@quendi.de>
2024-04-18 18:35:45 +0200

    Apply suggestions from code review
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- src/PrettyPrinting.jl
+++ src/PrettyPrinting.jl
@@ -2127,7 +2127,7 @@ for details.
 
 # Examples
 
-```repl
+```jldoctest
 julia> AbstractAlgebra.is_terse(stdout)
 false
 
@@ -2150,7 +2150,7 @@ for details.
 
 # Examples
 
-```repl
+```jldoctest
 julia> AbstractAlgebra.is_terse(stdout)
 false
 
