fb5ccfd78b30f162a32d28b491e249247d2b272b
Max Horn <max@quendi.de>
2024-09-24 22:00:50 +0200

    Change PolyRing to poly_ring

--- src/Poly.jl
+++ src/Poly.jl
@@ -2944,16 +2944,16 @@ function polynomial_to_power_sums(f::PolyRingElem{T}, n::Int=degree(f)) where T 
 end
 
 @doc raw"""
-    power_sums_to_polynomial(P::Vector{T};
-                     parent::PolyRing{T}=PolyRing(parent(P[1])) where T <: RingElement -> PolyRingElem{T}
+    power_sums_to_polynomial(P::Vector{T}) where T <: RingElement -> PolyRingElem{T}
+                     parent::PolyRing{T}=poly_ring(parent(P[1])) where T <: RingElement -> PolyRingElem{T}
 
 Uses the Newton (or Newton-Girard) identities to obtain the polynomial
 with given sums of powers of roots. The list must be nonempty and contain
 `degree(f)` entries where $f$ is the polynomial to be recovered. The list
 must start with the sum of first powers of the roots.
 """
-function power_sums_to_polynomial(P::Vector{T}; 
-                           parent::PolyRing{T}=PolyRing(parent(P[1]))) where T <: RingElement
+function power_sums_to_polynomial(P::Vector{T};
+                           parent::PolyRing{T}=poly_ring(parent(P[1]))) where T <: RingElement
    return power_sums_to_polynomial(P, parent)
 end
 
