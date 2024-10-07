7298a3d6cf717a40d854828b564ceffe670cf65d
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-10-07 13:46:43 +0200

    Clarify how to get all left/right inverses
    In docstring for `is_invertible_with_inverse`.
    
    Co-authored-by: Max Horn <max@quendi.de>
    Co-authored-by: Tommy Hofmann <thofma@gmail.com>

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -3553,11 +3553,15 @@ end
 @doc raw"""
     is_invertible_with_inverse(A::MatrixElem{T}; side::Symbol = :left) where {T <: RingElement}
 
-Given an $n\times m$ matrix $A$ over a ring, return a tuple `(flag, B)`.
-If `side` is `:right` and `flag` is true, $B$ is the right inverse of $A$
-i.e. $AB$ is the $n\times n$ unit matrix. If `side` is `:left` and `flag` is
-true, $B$ is the left inverse of $A$ i.e. $BA$ is the $m\times m$ unit matrix.
-If `flag` is false, no right or left inverse exists.
+Given an $n \times m$ matrix $A$ over a ring, return a tuple `(flag, B)`. If
+`side` is `:right` and `flag` is true, $B$ is a right inverse of $A$ i.e. $A B$
+is the $n \times n$ unit matrix. If `side` is `:left` and `flag` is true, $B$
+is a left inverse of $A$ i.e. $B A$ is the $m \times m$ unit matrix. If `flag`
+is false, no right or left inverse exists.
+
+To get the space of all inverses, note that if $B$ and $C$ are both right
+inverses, then $A (B - C) = 0$, and similar for left inverses. Hence from one
+inverse one can find all by making suitable use of [`kernel`](@ref).
 """
 function is_invertible_with_inverse(A::MatrixElem{T}; side::Symbol = :left) where {T <: RingElement}
    if (side == :left && nrows(A) < ncols(A)) || (side == :right && ncols(A) < nrows(A))
