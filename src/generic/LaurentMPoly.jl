555a183a6fbc2bbc23f6d02ddb166046b3198ff2
Max Horn <max@quendi.de>
2024-10-09 22:55:55 +0200

    Strict parent checks in == methods

--- src/generic/LaurentMPoly.jl
+++ src/generic/LaurentMPoly.jl
@@ -134,7 +134,7 @@ end
 ###############################################################################
 
 function ==(a::LaurentMPolyWrap, b::LaurentMPolyWrap)
-    check_parent(a, b, false) || return false
+    check_parent(a, b)
     if a.mindegs == b.mindegs
         return a.mpoly == b.mpoly
     end
