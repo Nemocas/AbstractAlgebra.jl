caa12ae970529468644a4b84abb616e56ac81cbe
Max Horn <max@quendi.de>
2024-10-09 22:55:55 +0200

    Strict parent checks in == methods

--- src/RelSeries.jl
+++ src/RelSeries.jl
@@ -731,8 +731,7 @@ that power series to different precisions may still be arithmetically
 equal to the minimum of the two precisions.
 """
 function ==(x::RelPowerSeriesRingElem{T}, y::RelPowerSeriesRingElem{T}) where T <: RingElement
-   b = check_parent(x, y, false)
-   !b && return false
+   check_parent(x, y)
 
    xval = valuation(x)
    xprec = precision(x)
