bcdb3c353ec9f3a1a67314ed0f914e179803a173
John Abbott <John.Abbott@rptu.de>
2024-12-18 12:25:02 +0100

    Add impls of is_unit & is_nilpotent

--- src/NCPoly.jl
+++ src/NCPoly.jl
@@ -783,3 +783,37 @@ polynomial_ring_only(R::T, s::Symbol; cached::Bool=true) where T<:NCRing =
 # Simplified constructor
 
 PolyRing(R::NCRing) = polynomial_ring_only(R, :x; cached=false)
+
+
+
+###############################################################################
+#
+#   is_unit & is_nilpotent
+#
+###############################################################################
+
+# ASSUMES structural interface is analogous to that for univariate polynomials
+
+function is_unit(f::T) where {T<:NCPolyRingElem}
+  is_unit(constant_coefficient(f)) || return false
+  characteristic(parent(f)) == 1 && return true  # coeffs in zero ring
+  is_domain_type(elem_type(coefficient_ring(f))) && return (length(f)==1)
+  for i in 1:degree(f)   # ASSUMES constant_coefficient has index 0
+    if !is_nilpotent(coeff(f, i))
+      return false
+    end
+  end
+  return true
+end
+
+
+function is_nilpotent(f::T) where {T<:NCPolyRingElem}
+  is_domain_type(elem_type(coefficient_ring(f))) && return (is_zero(f))
+  for i in 0:degree(f)
+    if !is_nilpotent(coeff(f, i))
+      return false
+    end
+  end
+  return true
+end
+
