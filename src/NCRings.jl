78197c9bdbcc8dc0a54ad8fcc1e70b92434f5fdc
Max Horn <max@quendi.de>
2024-12-21 02:05:05 +0100

    Apply suggestions from code review

--- src/NCRings.jl
+++ src/NCRings.jl
@@ -163,10 +163,10 @@ Return true iff $a$ is nilpotent, i.e. a^k == 0 for some k.
 # Examples
 ```jldoctest
 julia> ZZ720,_ = residue_ring(ZZ,720);
+
 julia> S, x = polynomial_ring(QQ, :x);
 
 julia> is_nilpotent(30*x), is_nilpotent(30+90*x), is_nilpotent(S(15))
-
 (true, true, false)
 ```
 """
