ed704900f41ac5f49512f4e94b26afa1065d7546
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-03-12 23:31:11 +0100

    Add docstring

--- src/Groups.jl
+++ src/Groups.jl
@@ -316,6 +316,22 @@ function isomorphism(::Type{T}, G::T; on_gens::Bool=false) where T <: Group
   end::AbstractAlgebra.Generic.IdentityMap{T}
 end
 
+"""
+    isomorphism(::Type{T}, G::Group; on_gens=false) where T <: Group
+
+Return an isomorphism from `G` to a group `H` of type `T`.
+An exception is thrown if no such isomorphism exists.
+
+If `on_gens` is `true` then `gens(G)` is guaranteed to correspond to
+`gens(H)`;
+an exception is thrown if this is not possible.
+
+Isomorphisms are cached in `G`, subsequent calls of `isomorphism` with the
+same `T` (and the same value of `on_gens`) yield identical results.
+
+If only the image of such an isomorphism is needed, use `T(G)`;
+but this will assume `on_gens=false`.
+"""
 function isomorphism(::Type{T}, G::Group; on_gens::Bool=false) where T <: Group
   throw(NotImplementedError(:isomorphism, T, G))
 end
