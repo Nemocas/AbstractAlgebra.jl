c1cce0f37badd4f9919fc9ff04f0ea5e85868d54
Max Horn <max@quendi.de>
2025-05-01 12:53:22 +0200

    Fix a typo

--- src/MPoly.jl
+++ src/MPoly.jl
@@ -104,7 +104,7 @@ function vars(p::MPolyRingElem{T}) where {T <: RingElement}
 end
 
 @doc raw"""
-    var_index(p::MPolyRingElem{T}) where {T <: RingElement}
+    var_index(x::MPolyRingElem{T}) where {T <: RingElement}
 
 Return the index of the given variable $x$. If $x$ is not a variable
 in a multivariate polynomial ring, an exception is raised.
