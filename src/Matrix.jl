79f1f23822be8bf46638dee5d499431aa63c5e74
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-05-15 19:22:41 +0200

    Apply comments
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -2688,20 +2688,16 @@ end
 ###############################################################################
 
 """
-    is_alternating(M::MatElem)
+    is_alternating(M::MatrixElem)
 
 Return whether the form corresponding to the matrix `M` is alternating,
 i.e. `M = -transpose(M)` and `M` has zeros on the diagonal.
 Return `false` if `M` is not a square matrix.
 """
 function is_alternating(M::MatrixElem)
-  n = nrows(M)
-  n == ncols(M) || return false
-  for i in 1:n
+  is_skew_symmetric(M) || return false
+  for i in 1:nrows(M)
     is_zero_entry(M, i, i) || return false
-    for j in (i + 1):n
-      M[i, j] == -M[j, i] || return false
-    end
   end
   return true
 end
