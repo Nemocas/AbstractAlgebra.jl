84a2bd6a7a4c3defbd58deff1d0ea002baa04b41
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-05-15 20:42:47 +0200

    Move parts of Oscar's `MoveToAbstractAlgebra.jl` over (#2082)
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -2688,6 +2688,21 @@ end
 ###############################################################################
 
 """
+    is_alternating(M::MatrixElem)
+
+Return whether the form corresponding to the matrix `M` is alternating,
+i.e. `M = -transpose(M)` and `M` has zeros on the diagonal.
+Return `false` if `M` is not a square matrix.
+"""
+function is_alternating(M::MatrixElem)
+  is_skew_symmetric(M) || return false
+  for i in 1:nrows(M)
+    is_zero_entry(M, i, i) || return false
+  end
+  return true
+end
+
+"""
     is_skew_symmetric(M::MatrixElem)
 
 Return `true` if the given matrix is skew symmetric with respect to its main
