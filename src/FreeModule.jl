6f932ccfb0c4e102031421f28fdb0980915eddb6
Miķelis Emīls Miķelsons <mikelis.emils.mikelsons@gmail.com>
2025-05-31 01:13:12 +0200

    Add vector_space_dim documentation (#2102)

--- src/FreeModule.jl
+++ src/FreeModule.jl
@@ -34,4 +34,33 @@ function vector_space(R::Field, dim::Int; cached::Bool = true)
    Generic.FreeModule(R, dim; cached=cached)
 end
 
+@doc raw"""
+    vector_space_dim(M::FPModule)
+
+Return the dimension of the given vector space.
+
+# Examples
+```jldoctest
+julia> M = free_module(QQ, 2)
+Vector space of dimension 2 over rationals
+
+julia> vector_space_dim(M)
+2
+
+julia> m = M([1, 2])
+(1//1, 2//1)
+
+julia> N, = sub(M, [m])
+(Subspace over rationals with 1 generator and no relations, Hom: N -> M)
+
+julia> vector_space_dim(N)
+1
+
+julia> Q, = quo(M, N)
+(Quotient space over rationals with 1 generator and no relations, Hom: M -> Q)
+
+julia> vector_space_dim(Q)
+1
+```
+"""
 function vector_space_dim end
