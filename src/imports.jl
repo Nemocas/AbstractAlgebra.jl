2ae9ed4e112f288c2a1d103ba2304007beaea2f1
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-07-11 10:32:44 +0200

    Clean up Pkg extension handling

--- src/imports.jl
+++ src/imports.jl
@@ -1,10 +1,6 @@
 using Random: Random, AbstractRNG, SamplerTrivial
 using RandomExtensions: RandomExtensions, make, Make, Make2, Make3, Make4
 
-if !isdefined(Base, :get_extension)
-  using Requires: @require
-end
-
 import LinearAlgebra
 
 import LinearAlgebra: det
