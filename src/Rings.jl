dd9c38639bb31e287c27cd0ca51c35244b0253fb
Max Horn <max@quendi.de>
2025-07-14 13:16:08 +0200

    Fix generic is_noetherian(::Module) method

--- src/Rings.jl
+++ src/Rings.jl
@@ -265,7 +265,7 @@ is_noetherian(::Integers) = true
 is_noetherian(R::Union{PolyRing, MPolyRing, LaurentPolyRing, LaurentMPolyRing}) = is_noetherian(coefficient_ring(R))
 is_noetherian(R::Union{MSeriesRing, SeriesRing}) = is_noetherian(base_ring(R))
 is_noetherian(R::ResidueRing) = is_noetherian(base_ring(R)) || throw(NotImplementedError(:is_noetherian, R))
-is_noetherian(M::Module) = is_noetherian(base_ring(M)) || throw(NotImplementedError(:is_noetherian, M))
+is_noetherian(M::Module) = (is_noetherian(base_ring(M)) && is_finite(ngens(M))) || throw(NotImplementedError(:is_noetherian, M))
 
 @doc raw"""
     krull_dim(R::Ring)
