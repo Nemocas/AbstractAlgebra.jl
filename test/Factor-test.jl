3a7f9dcf90370c6ef958350734fba7ae1f0a2b3f
Tommy Hofmann <thofma@gmail.com>
2025-09-01 21:57:04 +0200

    feat: store (additional) array in Fac (#2146)

--- test/Factor-test.jl
+++ test/Factor-test.jl
@@ -1,14 +1,21 @@
 @testset "Fac.constructors" begin
    f = Fac(-1, Dict{Int, Int}(2 => 3, 3 => 1))
+   ff = Fac(-1, [2 => 3, 3 => 1])
 
    @test -24 == evaluate(f)
+   @test -24 == evaluate(ff)
    @test -24 == unit(f)*prod([p^e for (p, e) in f])
+   @test -24 == unit(ff)*prod([p^e for (p, e) in ff])
+
+   @test collect(ff) == [2 => 3, 3 => 1]
 end
 
 @testset "Fac.printing" begin
    f = Fac(-1, Dict{Int, Int}(2 => 3, 3 => 1))
+   ff = Fac(-1, [(2, 3), (3, 1)])
 
    @test string(f) == "-1 * 2^3 * 3" || string(f) == "-1 * 3 * 2^3"
+   @test string(ff) == "-1 * 2^3 * 3"
 
    @test string(Fac{BigInt}()) isa String
 
