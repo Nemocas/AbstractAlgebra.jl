27c43f3f43dad8e19934b707724b48f1d007f240
Martin Wagner <martin.wagner.dev@gmail.com>
2025-10-09 15:44:09 +0200

    Add `coefficient_ring` for modules and improve the docs for `base_ring`

--- src/generic/FreeModule.jl
+++ src/generic/FreeModule.jl
@@ -16,6 +16,10 @@ base_ring_type(::Type{FreeModule{T}}) where T <: Union{RingElement, NCRingElem} 
 
 base_ring(M::FreeModule{T}) where T <: Union{RingElement, NCRingElem} = M.base_ring::parent_type(T)
 
+coefficient_ring_type(T::Type{FreeModule}) = base_ring_type(T)
+
+coefficient_ring(M::FreeModule) = base_ring(M)
+
 elem_type(::Type{FreeModule{T}}) where T <: Union{RingElement, NCRingElem} = FreeModuleElem{T}
 
 parent(m::FreeModuleElem{T}) where T <: Union{RingElement, NCRingElem} = m.parent
