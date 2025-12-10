aed9edf97041eb9e81a13d8da7ba4ecac3021a30
Max Horn <max@quendi.de>
2025-12-10 23:26:49 +0100

    Fix is_power for e.g. ZZPolyRing
    Without this, we get in Nemo:
    
        julia> ZZx, x = ZZ[:x];  is_power(2^2 * x^2, 2)
        (true, 4*x)
    
    With it, the correct result is produced:
    
        julia> ZZx, x = ZZ[:x];  is_power(2^2 * x^2, 2)
        (true, 2*x)

--- src/generic/Misc/Poly.jl
+++ src/generic/Misc/Poly.jl
@@ -9,10 +9,10 @@ function is_power(a::PolyRingElem, n::Int)
     # probably a equal-degree-factorisation would be good + some more gcd's
     # implement some Newton-type algo?
     degree(a) % n == 0 || return false, a
-    fl, x = is_power(leading_coefficient(a), n)
-    fl || return false, a
     f = factor(a)
     all(i % n == 0 for (_, i) in f) || return false, a
+    fl, x = is_power(constant_coefficient(f.unit), n)
+    fl || return false, a
     return true, x*prod(p^div(k, n) for (p, k) = f)
 end
 
