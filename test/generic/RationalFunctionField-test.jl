6b947a56205c26e19036245e096ee31a6a2efecf
Tommy Hofmann <thofma@gmail.com>
2026-01-16 11:02:41 +0100

    Update test/generic/RationalFunctionField-test.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- test/generic/RationalFunctionField-test.jl
+++ test/generic/RationalFunctionField-test.jl
@@ -593,7 +593,7 @@ end
    @test AbstractAlgebra.promote_rule(T, T) == T
 end
 
-@testset "Generic.RationalFunctionField.evaluation_set" begin
+@testset "Generic.RationalFunctionField.evaluation_points" begin
   let
     K, x = rational_function_field(QQ, "x")
     S = AbstractAlgebra.evaluation_points(K, 10)
