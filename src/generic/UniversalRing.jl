214e01ac6865555545e926f9b5f0e0e51ded3f5f
Martin Wagner <martin.wagner.dev@gmail.com>
2026-02-24 13:02:28 +0100

    UniversalRing: Add basic documentation

--- src/generic/UniversalRing.jl
+++ src/generic/UniversalRing.jl
@@ -8,6 +8,6 @@ function ConformanceTests.generate_element(R::UniversalRing{MPoly{EuclideanRingR
    return rand(R, 0:4, 0:10, -10:10)
 end
 
-function ConformanceTests.generate_element(R::UniversalRing{LaurentMPolyWrap{EuclideanRingResidueRingElem{BigInt}}})
-   return rand(S, 0:4, -10:10, -10:10)
+function ConformanceTests.generate_element(R::UniversalRing{<:LaurentMPolyWrap{EuclideanRingResidueRingElem{BigInt}}})
+   return rand(R, 0:4, -10:10, -10:10)
 end
