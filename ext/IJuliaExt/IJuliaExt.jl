c2f6354f426a623a01139f849b32c7a640953605
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-03-04 14:35:09 +0100

    Address comment

--- ext/IJuliaExt/IJuliaExt.jl
+++ ext/IJuliaExt/IJuliaExt.jl
@@ -3,6 +3,10 @@ module IJuliaExt
 using AbstractAlgebra
 using IJulia
 
-AbstractAlgebra._set_ijulia_inited_hook!(() -> IJulia.inited)
+function __init__()
+  # this needs to be called in __init__ as it modifies a global variable in another package
+  # (AbstractAlgebra is considered a different package than this extension for precompilation purposes)
+  AbstractAlgebra._set_ijulia_inited_hook!(() -> IJulia.inited)
+end
 
 end # module
