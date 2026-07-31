c239eb1a1984014c6f0a6798b87c9de77b3b6bfd
Martin Bies <HereAround@users.noreply.github.com>
2026-07-31 10:23:30 +0200

    Overhaul Linear Algebra documentation (#2448)
    Co-authored-by: JohnAAbbott <124266874+JohnAAbbott@users.noreply.github.com>

--- src/NCRings.jl
+++ src/NCRings.jl
@@ -284,6 +284,7 @@ end
 Return an array $M$ of "powers" of `a` where $M[i + 1] = a^i$ for $i = 0..d$.
 
 # Examples
+
 ```jldoctest
 julia> M = ZZ[1 2 3; 2 3 4; 4 5 5]
 [1   2   3]
@@ -297,7 +298,6 @@ julia> A = powers(M, 4)
  [17 23 26; 24 33 38; 34 48 57]
  [167 233 273; 242 337 394; 358 497 579]
  [1725 2398 2798; 2492 3465 4044; 3668 5102 5957]
-
 ```
 """
 function powers(a::T, d::Int) where {T <: Union{NCRingElement, MatElem}}
